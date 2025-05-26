d84e71efe616d35ac21e7626b8d97cc86dac2a3e
Shreyash-badole17 <142210124+Shreyash-badole17@users.noreply.github.com>
2025-05-26 16:51:03 +0530

    Time: 0 ms (100%), Space: 53.4 MB (21.92%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,7 +1,22 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        int n=nums.length;
-        return nums[n/2];
+        // Arrays.sort(nums);
+        // int n=nums.length;
+        // return nums[n/2];
+
+        int val = me(nums, nums[0], 0);
+    return val;}
+
+//using recursions
+     int me(int[] arr, int val,int index){
+        int count =0;
+        for(int i =index;i<arr.length;i++){
+            if(arr[i] == val)
+                count++;
+            else count--;
+            if(count<0) return me(arr, arr[index], i);
+        }
+        return val;
+    
     }
 }
