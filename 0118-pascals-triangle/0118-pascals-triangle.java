4612234042d319791545c9b07c18c3931bdc87f6
Shreyash-badole17 <142210124+Shreyash-badole17@users.noreply.github.com>
2025-05-30 16:42:13 +0530

    Time: 1 ms (84.74%), Space: 42.2 MB (40.8%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,25 +1,20 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> triangle=new ArrayList<List<Integer>>();
-        List<Integer> prevRow = new ArrayList<Integer>();
-        if(numRows>0){
-            prevRow.add(1);
-            triangle.add(prevRow);
-            if(numRows==1){
-                return triangle;
-            }
+        List<List<Integer>> map = new ArrayList<>();
+        if(numRows<0){
+           return map; 
         }
-        
+        map.add(new ArrayList<>(List.of(1)));
         for(int i=1;i<numRows;i++){
-            prevRow=triangle.get(i-1);
-            List<Integer> currRow = new ArrayList<Integer>();
-            currRow.add(1);
-            for(int j=1;j<prevRow.size();j++){
-                currRow.add(prevRow.get(j-1) + prevRow.get(j));
+            List<Integer> prevRow = map.get(i-1);
+            List<Integer> newRow = new ArrayList<>();
+            newRow.add(1);
+            for(int j=1;j<i;j++){
+                newRow.add(prevRow.get(j-1)+prevRow.get(j));
             }
-            currRow.add(1);
-            triangle.add(currRow);
+            newRow.add(1);
+            map.add(newRow);
         }
-        return triangle;
+        return map;
     }
 }
