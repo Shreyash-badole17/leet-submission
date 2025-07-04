e2999dcd98499ed084a2b502942d4333a1d7a417
Shreyash-badole17 <142210124+Shreyash-badole17@users.noreply.github.com>
2025-07-04 15:22:33 +0530

    Time: 0 ms (100%), Space: 42.5 MB (38.38%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,20 +1,41 @@
-class Solution {
-    public double myPow(double x, int n) {
-        long N = n;
-        if (N < 0) {
-            x = 1 / x;
-            N = -N;
-        }
-        return fastPow(x, N);
-    }
+// class Solution {
+//     public double myPow(double x, int n) {
+//         long N = n;
+//         if (N < 0) {
+//             x = 1 / x;
+//             N = -N;
+//         }
+//         return fastPow(x, N);
+//     }
 
-    private double fastPow(double x, long n) {
-        if (n == 0) return 1.0;
-        double half = fastPow(x, n / 2);
-        if (n % 2 == 0) {
-            return half * half;
-        } else {
-            return half * half * x;
+//     private double fastPow(double x, long n) {
+//         if (n == 0) return 1.0;
+//         double half = fastPow(x, n / 2);
+//         if (n % 2 == 0) {
+//             return half * half;
+//         } else {
+//             return half * half * x;
+//         }
+//     }
+// }
+
+class Solution{
+    public double myPow(double x, int n){
+        long N=n;
+        if(N<0){
+            x=1/x;
+            N=-N;
         }
+
+        double result=1.0;
+
+        while(N>0){
+            if((N&1)==1){
+                result*=x;
+            }
+            x*=x;
+            N=N>>1;
+        }
+        return result;
     }
 }
