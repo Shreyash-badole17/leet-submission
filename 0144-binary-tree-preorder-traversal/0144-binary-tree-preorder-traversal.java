b4c24de206df5a7d3450aab49377cc49ae0d91ff
Shreyash-badole17 <142210124+Shreyash-badole17@users.noreply.github.com>
2025-10-16 16:44:20 +0530

    Time: 0 ms (100%), Space: 41.6 MB (84.07%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -15,14 +15,37 @@
  */
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
-        List<Integer> ls= new ArrayList<>();
-        traverse(root,ls);
+        List<Integer> ls = new ArrayList<>();
+        if(root==null){
+            return ls;
+        }
+        Stack<TreeNode> st = new Stack<>();
+        st.push(root);
+        while(!st.isEmpty()){
+            TreeNode curr = st.pop();
+            ls.add(curr.val);
+            if(curr.right!=null){
+                st.push(curr.right);
+            }
+            if(curr.left!=null){
+                st.push(curr.left);
+            }
+        }
         return ls;
     }
-    private void traverse(TreeNode root, List<Integer> ls){
-        if(root==null) return;
-        ls.add(root.val);
-        traverse(root.left,ls);
-        traverse(root.right,ls);
-    }
 }
+
+//-- Recursive solution--
+// class Solution {
+//     public List<Integer> preorderTraversal(TreeNode root) {
+//         List<Integer> ls= new ArrayList<>();
+//         traverse(root,ls);
+//         return ls;
+//     }
+//     private void traverse(TreeNode root, List<Integer> ls){
+//         if(root==null) return;
+//         ls.add(root.val);
+//         traverse(root.left,ls);
+//         traverse(root.right,ls);
+//     }
+// }
